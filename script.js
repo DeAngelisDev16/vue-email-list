16a033c14a84fb786f849478993f0ecc2b35e336
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-21 20:02:38 +0100

    Genero una mail randomica nel dom

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        randomEmail:null,
+        
       
     }
   },
   methods:{
-    getRandomMail(){
+    getRandomEmail(){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-          .then(function (response) {
+          .then( (response) => {
               console.log(response.data.response);
+              this.randomEmail = response.data.response;
             
           })
 
@@ -20,7 +23,7 @@ createApp({
     
   },
   created(){
-    this.getRandomMail();
+    this.getRandomEmail();
    
 
   }
